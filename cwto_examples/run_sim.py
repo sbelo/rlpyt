6276cf10b52c028bb3533014f767aee89aa0090a
sbelo <38404402+sbelo@users.noreply.github.com>
2020-10-25 19:00:30 +0200

    Update run_sim.py

--- cwto_examples/run_sim.py
+++ cwto_examples/run_sim.py
@@ -1,6 +1,8 @@
 from rlpyt.cwto_samplers.serial.sampler import SerialSampler
 from rlpyt.cwto_samplers.parallel.cpu.sampler import CpuSampler
 from rlpyt.algos.pg.ppo import PPO
+from rlpyt.algos.qpg.sac import SAC
+from rlpyt.agents.qpg.sac_agent_beta import SacAgentBeta
 from rlpyt.cwto_runners.minibatch_rl import MinibatchRl, MinibatchRlEval
 from rlpyt.utils.logging.context import logger_context
 from rlpyt.spaces.int_box import IntBox
@@ -9,6 +11,7 @@ import torch
 from heparin_env import HeparinEnv
 import pickle
 import gym
+from rlpyt.models.qpg.mlp import QofMuMlpModel, PiMlpModel
 import whynot as wn
 from gym.spaces.box import Box
 from rlpyt.cwto_agents.cwto_agent_wrp import *
@@ -81,10 +84,10 @@ def build_and_train(game="cartpole", run_ID=0, cuda_idx=None, sample_mode="seria
     elif game == "heparin":
         work_env = HeparinEnv
         env_name = 'Heparin-Simulator'
-        state_space_low = np.asarray([18728.926,72.84662,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
-        state_space_high = np.asarray([2.7251439e+04,1.0664291e+02,200.0,8.9383472e+02,1.0025734e+02,1.5770737e+01,4.7767456e+01,1.0,1.0,1.0,1.0,1.0,1.0,1.0])
-        # state_space_low = np.asarray([18728.926,72.84662,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
-        # state_space_high = np.asarray([2.7251439e+04,1.0664291e+02,0.0000000e+00,8.9383472e+02,1.4476662e+02,1.3368750e+02,1.6815166e+02,1.0025734e+02,1.5770737e+01,4.7767456e+01,7.7194958e+00,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0])
+        state_space_low = np.asarray([0.0,0.0,0.0,0.0,0.0,0.0,0.0,18728.926,72.84662,0.0,0.0,0.0,0.0,0.0])
+        state_space_high = np.asarray([1.0,1.0,1.0,1.0,1.0,1.0,1.0,2.7251439e+04,1.0664291e+02,200.0,8.9383472e+02,1.0025734e+02,1.5770737e+01,4.7767456e+01])
+        # state_space_low = np.asarray([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,18728.926,72.84662,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0])
+        # state_space_high = np.asarray([1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,2.7251439e+04,1.0664291e+02,0.0000000e+00,8.9383472e+02,1.4476662e+02,1.3368750e+02,1.6815166e+02,1.0025734e+02,1.5770737e+01,4.7767456e+01,7.7194958e+00])
         obs_space = Box(state_space_low, state_space_high, dtype=np.float32)
         player_act_space = work_env(env_name).action_space
         if inc_player_last_act:
@@ -99,8 +102,33 @@ def build_and_train(game="cartpole", run_ID=0, cuda_idx=None, sample_mode="seria
         max_episode_length = 20
         player_model_kwargs = dict(hidden_sizes = [32],lstm_size = 16,nonlinearity = torch.nn.ReLU, normalize_observation = False, norm_obs_clip = 10, norm_obs_var_clip = 1e-6)
         observer_model_kwargs = dict(hidden_sizes = [128],lstm_size = 16,nonlinearity = torch.nn.ReLU, normalize_observation = False, norm_obs_clip = 10, norm_obs_var_clip = 1e-6)
-
-
+  
+    elif game == "halfcheetah":
+        work_env = gym.make
+        env_name = 'HalfCheetah-v2'
+        temp_env = work_env(env_name)
+        state_space_low = np.concatenate([np.zeros(temp_env.observation_space.low.shape),temp_env.observation_space.low])
+        state_space_high = np.concatenate([np.ones(temp_env.observation_space.high.shape),temp_env.observation_space.high])
+        obs_space = Box(state_space_low, state_space_high, dtype=np.float32)
+        player_act_space = temp_env.action_space
+        if inc_player_last_act:
+            observer_obs_space = Box(np.append(state_space_low,0), np.append(state_space_high,4), dtype=np.float32)
+        else:
+            observer_obs_space = obs_space
+        player_reward_shaping = None
+        observer_reward_shaping = None
+        temp_env.close()
+        max_decor_steps = 10
+        b_size = 20
+        num_envs = 8
+        max_episode_length = np.inf
+        player_model_kwargs = dict(hidden_sizes = [256,256])
+        observer_model_kwargs = dict(hidden_sizes = [256,256])
+        player_q_model_kwargs = dict(hidden_sizes = [256,256])
+        observer_q_model_kwargs = dict(hidden_sizes = [256,256])
+        player_v_model_kwargs = dict(hidden_sizes = [256,256])
+        observer_v_model_kwargs = dict(hidden_sizes = [256,256])
+        
     if serial:
         n_serial = int(len(state_space_high) / 2)
         observer_act_space = Discrete(2)
@@ -150,14 +178,19 @@ def build_and_train(game="cartpole", run_ID=0, cuda_idx=None, sample_mode="seria
         eval_env_kwargs = eval_env_kwargs,
         eval_max_steps = eval_max_steps,
     )
+    if game == 'halfcheeth:
+        player_algo = SAC()
+        observer_algo = SAC()
+        player = SacAgentBeta(ModelCls=PiMlpModel, QModelCls=QofMuMlpModel, model_kwargs=player_model_kwargs, q_model_kwargs=player_q_model_kwargs, v_model_kwargs=player_v_model_kwargs)
+        observer = SacAgentBeta(ModelCls=PiMlpModel, QModelCls=QofMuMlpModel, model_kwargs=observer_model_kwargs, q_model_kwargs=observer_q_model_kwargs, v_model_kwargs=observer_v_model_kwargs)
+    else:
+        player_model = CWTO_LstmModel
+        observer_model = CWTO_LstmModel
 
-    player_model = CWTO_LstmModel
-    observer_model = CWTO_LstmModel
-
-    player_algo = PPO()
-    observer_algo = PPO()
-    player = CWTO_LstmAgent(ModelCls=player_model, model_kwargs=player_model_kwargs, initial_model_state_dict=None)
-    observer = CWTO_LstmAgent(ModelCls=observer_model, model_kwargs=observer_model_kwargs, initial_model_state_dict=None)
+        player_algo = PPO()
+        observer_algo = PPO()
+        player = CWTO_LstmAgent(ModelCls=player_model, model_kwargs=player_model_kwargs, initial_model_state_dict=None)
+        observer = CWTO_LstmAgent(ModelCls=observer_model, model_kwargs=observer_model_kwargs, initial_model_state_dict=None)
     if one_agent:
         agent = CWTO_AgentWrapper(player, observer, serial=serial, n_serial=n_serial, alt=alt, train_mask=train_mask,one_agent=one_agent,nplayeract=player_act_space.n)
     else:
@@ -180,7 +213,10 @@ def build_and_train(game="cartpole", run_ID=0, cuda_idx=None, sample_mode="seria
         alt_train=alt_train
     )
     config = dict(domain=game)
-    name = "ppo_" + game
+    if game == 'halfcheetah':
+        name = "sac_" + game
+    else:
+        name = "ppo_" + game
     log_dir = os.getcwd() + "/cwto_logs/" + name
     with logger_context(log_dir, run_ID, name, config):
         runner.train()
@@ -230,7 +266,7 @@ if __name__ == "__main__":
     import wandb
 
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--game', help='name of env', default='hiv', type=str, choices=['cartpole','hiv','heparin'])
+    parser.add_argument('--game', help='name of env', default='hiv', type=str, choices=['cartpole','hiv','heparin','halfcheetah'])
     parser.add_argument('--run_ID', help='run identifier (logging)', type=int, default=0)
     parser.add_argument('--cuda_idx', help='gpu to use ', type=int, default=None)
     parser.add_argument('--secondary_cuda_idx', help='gpu to use - for parallel work', type=int, default=None)
